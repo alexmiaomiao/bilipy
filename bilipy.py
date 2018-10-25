5a689a0f82e36ca58897e623eedbd3b9844a139a
alexmiaomiao <2564272712@qq.com>
2018-10-25 19:39:54 +0800

    add merge and split downloader class

--- bilipy.py
+++ bilipy.py
@@ -4,123 +4,30 @@ import re
 import json
 import sys
 import threading
-import time
 
 import requests
 from bs4 import BeautifulSoup
 import progressbar
 
-
-class Downloader():
-    def __init__(self, agent, name, size, headers, thread_num):
-        self.dsize = 0
-        self.dsize_lock = threading.Lock()
-        self.agent = agent
-        self.name = name
-        self.size = size
-        self.headers = headers
-        self.thread_num = thread_num
-
-    def _show(self):
-        with progressbar.ProgressBar(max_value=size) as bar:
-            while True:
-                bar.update(self.dsize)
-                if self.dsize == size:
-                    return
-                time.sleep(0.5)
-
-    def _download(self, k):
-        raise NotImplementedError()
-
-    def _write(self, r, name, start):
-        with open(name, 'wb+') as f:
-            f.seek(start)
-            for data in r.iter_content(chunk_size=1024*1024):
-                f.write(data)
-                with self.dsize_lock:
-                    self.dsize += len(data)
-
-
-    def start(self):
-        threads = [threading.Thread(target=self._download, args=(i,)) for i in range(self.thread_num)]
-        threads.append(threading.Thread(target=self._show))
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join()
-
-
-class SingleDownloader(Downloader):
-    def __init__(self, durl, agent, name, size, headers, thread_num = 10):
-        super().__init__(agent, name, size, headers, thread_num)
-        self.durl = durl
-        self.frag = self.size // self.thread_num
-
-    def _download(self, k):
-        start = self.frag * k
-        end = self.frag * (k + 1) - 1
-        headers = self.headers.copy()
-        if k == self.thread_num - 1:
-            headers['range'] = 'bytes=%s-' % start
-        else:
-            headers['range'] = 'bytes=%s-%s' % (start, end)
-        url = self.durl['backup_url'][0]
-
-        with self.agent.get(url, headers=headers, stream=True, verify=False) as r:
-            if r.status_code == 200 or 206:
-                self._write(r, self.name, start)
-
-
-class MultiDownloader(Downloader):
-    def __init__(self, durls, agent, name, size, headers, thread_num = 10):
-        super().__init__(agent, name, size, headers, thread_num)
-        self.durls = durls
-        if self.thread_num <= len(durls) * 2:
-            self.durl_thread_num = 2
-        else:
-            self.durl_thread_num = thread_num // len(durls)
-        self.thread_num = self.durl_thread_num * len(durls)
-
-    def _download(self, k):
-        order = k // self.durl_thread_num
-        frag = self.durls[order]['size'] // self.durl_thread_num
-        dk = k % self.durl_thread_num
-        start = frag * dk
-        end = frag * (dk+1) - 1
-        headers = self.headers.copy()
-        if dk == self.durl_thread_num - 1:
-            headers['range'] = 'bytes=%s-' % start
-        else:
-            headers['range'] = 'bytes=%s-%s' % (start, end)
-        url = self.durls[order]['backup_url'][0]
-
-        with self.agent.get(url, headers=headers, stream=True, verify=False) as r:
-            if r.status_code == 200 or 206:
-                self._write(r, self.name + '.' + str(order), start)
-
-    def _merge(self):
-        pass
-
+import downloader
 
 url = sys.argv[1]
 
 headers = {}
 headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 headers['Referer'] = 'https://www.bilibili.com'
-s = requests.Session()
 
+s = requests.Session()
 r = s.get(url, headers=headers)
 soup = BeautifulSoup(r.text, features="html.parser")
 jss = soup.find(name="script", text=re.compile("^window.__playinfo__"))
 playinfo = json.loads(jss.text[20:])
 durls = playinfo['durl']
 durls.sort(key=lambda x: x['order'])
-print(durls)
 size = sum(durl['size'] for durl in durls)
-name = soup.find(name="title").text.split("_哔哩哔哩")[0] + '.' + playinfo['format']
+name = soup.find(name="title").text.split("_哔哩哔哩")[0] + '.' + durls[0]['url'].split('?')[0].split('.')[-1]
 
 if len(durls) == 1:
-    SingleDownloader(durls[0], s, name, size, headers).start()
+    downloader.SingleDownloader(durls[0], s, name, size, headers).start()
 else:
-    MultiDownloader(durls, s, name, size, headers).start()
-
+    downloader.MultiDownloader(durls, s, name, size, headers).start()
